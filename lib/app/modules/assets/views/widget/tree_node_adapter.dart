a97eb3d41814aae327d8442f99af18515d15757f
allyssonmastrangelo <allysson-m@hotmail.com>
2024-06-25 12:10:43 -0300

    refactory tree_node_adapter.dart

--- lib/app/modules/assets/views/widget/tree_node_adapter.dart
+++ lib/app/modules/assets/views/widget/tree_node_adapter.dart
@@ -12,28 +12,35 @@ class TreeNodeWidget extends StatelessWidget {
     return _buildNode(node);
   }
 
+  Widget _buildIcon(IconData? iconData) {
+    return iconData != null ? Icon(iconData) : Container();
+  }
+
   Widget _buildNode(NodeTree node) {
     var iconData = {
       'operating': Icons.hourglass_empty,
       'alert': Icons.warning_amber
     };
+
+    Widget leadingIcon = _buildIcon(node.icon);
+    Widget? subtitle = node.sensorType != null ? Text(node.sensorType!) : null;
+    Widget? trailing = node.status != null ? Icon(iconData[node.status]) : null;
+
     if (node.children.isEmpty) {
       return ListTile(
         title: Text(node.label),
-        leading:
-            node.icon != null ? Icon(node.icon) : null,
-        subtitle: node.sensorType != null ? Text(node.sensorType!) : null,
-        trailing: node.status != null ? Icon(iconData[node.status]) : null,
+        leading: leadingIcon,
+        subtitle: subtitle,
+        trailing: trailing,
       );
     }
 
     return ExpansionTile(
       key: PageStorageKey<NodeTree>(node),
       title: Text(node.label),
-      leading:
-          node.icon != null ? Icon(node.icon) : null,
-      subtitle: node.sensorType != null ? Text(node.sensorType!) : null,
-      trailing: node.status != null ? Text(node.status!) : null,
+      leading: leadingIcon,
+      subtitle: subtitle,
+      trailing: trailing,
       children: node.children.map(_buildNode).toList(),
     );
   }
